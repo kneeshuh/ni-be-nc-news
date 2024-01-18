ed9757a78fc27706c37695049fe66769b62da311
kneeshuh <ni.gettings@gmail.com>
2024-01-18 15:35:57 +0000

    remove commented out code from test.sql

--- __tests__/test.sql
+++ __tests__/test.sql
@@ -1,11 +1,1 @@
 \c nc_news_test
-
-
--- SELECT * FROM articles WHERE topic = topic; <-- gets all articles that have a topic
-
--- SELECT articles.article_id, articles.title, articles.topic, articles.author, articles.body, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles
--- LEFT JOIN comments on comments.article_id = articles.article_id
--- WHERE articles.article_id = 2
--- GROUP BY articles.article_id;
-
-SELECT * FROM users;
