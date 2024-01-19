5202999727ca8b86ff60b63875f2467e3cc0960e
kneeshuh <ni.gettings@gmail.com>
2024-01-19 11:56:14 +0000

    remove hard coding for valid topic queries

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,4 +1,5 @@
 const { fetchArticleById, fetchAllArticles, updateVotesByArticleId } = require('../models/articles.models')
+const { checkTopicExists } = require('../utils/check-topic-exists')
 
 exports.getArticleById = (req, res, next) => {
     const { article_id } = req.params
@@ -12,27 +13,25 @@ exports.getArticleById = (req, res, next) => {
 
 exports.getAllArticles = (req, res, next) => {
     const { topic } = req.query;
-    const validTopicQueries = ['cats', 'mitch']
+    const queries = []
     if (!topic) {
-        fetchAllArticles().then((articles) => {
-            res.status(200).send({ articles })
-        })
-        .catch((err) => {
-            next(err)
-        })
-    } else if (!validTopicQueries.includes(topic)) {
-        return Promise.reject({ status: 404, msg: 'invalid topic query'})
-        .catch((err) => {
-            next (err)
-        })
-    } else {
-        fetchAllArticles(topic).then((articles) => {
-            res.status(200).send({ articles })
-        })
-        .catch((err) => {
-            next(err)
-        })
+        const fetchArticlesQuery = fetchAllArticles()
+        queries.push(fetchArticlesQuery)
     }
+    if (topic) {
+        const fetchArticlesWithTopicQuery = fetchAllArticles(topic)
+        queries.push(fetchArticlesWithTopicQuery)
+        const topicExistsQuery = checkTopicExists(topic)
+        queries.push(topicExistsQuery)
+    }
+    Promise.all(queries)
+    .then((response) => {
+        const articles = response[0]
+        res.status(200).send({ articles })
+    })
+    .catch((err) => {
+        next(err)
+    })
 }
 
 exports.patchVotesByArticleId = (req, res, next) => {
